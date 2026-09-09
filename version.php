7d6f7cc6ba3fc9d551c3f1c3843f1cde39d36764
Matt Dwen <matt@dwen.co.nz>
2026-09-09 16:01:31 +1200

    Set initial release

--- version.php
+++ version.php
@@ -4,6 +4,7 @@ defined('MOODLE_INTERNAL') || die();
 
 // This is the version of the plugin.
 $plugin->version = '2026042000';
+$plugin->release = '0.0.1';
 
 // This is the version of Moodle this plugin requires.
 $plugin->requires = '2026042000';
